3f60ed43b472a3f73a9befbc14c53833e9212d67
AlexArzhanov <arzhanovaleksandr1@gmail.com>
2023-03-19 13:37:56 +0300

    trying to use interface

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 
 func main() {
 
+	dataAccess.ServiceDataConn = &dataAccess.PostgresConnection{}
+
 	dataAccess.NewPostgresConnection("admin", "password", "localhost", 5432, "pgdb")
 
 	server := server.NewServer("", 8080)
